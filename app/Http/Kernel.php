e658beb2f5497d2927248999c59aa62ea52af8fd
Filip Sijak <filip.sijak@sykescottages.co.uk>
2020-10-20 12:41:09 +0100

    Adding initial jwt authentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'gameId' =>  \App\Http\Middleware\GameId::class,
+        'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
     ];
 
     /**
